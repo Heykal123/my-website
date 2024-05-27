1b2ebf633e2883a6621479192319a8dcf8078274
Heykal123 <165516771+Heykal123@users.noreply.github.com>
2024-05-27 19:42:27 +0300

    Add files via upload

---
+++ react-todo-list-app/src/context/TodoContext.jsx
@@ -0,0 +1,13 @@
+// implement TodoContext
+
+import React from "react";
+
+
+function TodoContext(){
+    return(
+        <div>
+            To Context
+        </div>
+    )
+}
+export default TodoContext;
